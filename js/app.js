a81f316a42d7def2f7a4f11a879f1b6a2e3e9ca1
Greco <greco.fer23gmail.com>
2026-08-02 00:25:50 -0300

    modal nuevo proyecto, debugging pages

--- js/app.js
+++ js/app.js
@@ -12,39 +12,51 @@ const modalCloseRegister = document.querySelector("#modal-close-register")
 
 const formRegister = document.querySelector("#form_register")
 
+const modalProject = document.querySelector("#modal-project")
+const newProject = document.querySelector("#open-project-modal")
 
 
 
 
-start.addEventListener("click", function(e) {
+if(start) {
+    start.addEventListener("click", function(e) {
     e.preventDefault()
     modalLogin.classList.add("active")
-})
+});
+}
 
-register.addEventListener("click", function(e) {
-    e.preventDefault()
-    modalRegister.classList.add("active")
-})
-
+if (register) {
+    register.addEventListener("click", function(e) {
+        e.preventDefault();
+        modalRegister.classList.add("active");
+    });
+}
+ 
+if(login) {
 login.addEventListener("click", function(e) {
     e.preventDefault()
     modalLogin.classList.add("active")
-})
+});
+}
 
 /* CERRAR MODAL CON LA "X"*/
-
+if(modalCloseLogin) {
 modalCloseLogin.addEventListener("click", function(e) {
     e.preventDefault()
     modalLogin.classList.remove("active")
-})
+});
+}
 
+if(modalCloseRegister) {
 modalCloseRegister.addEventListener("click", function(e) {
     e.preventDefault()
     modalRegister.classList.remove("active")
-})
+});
+}
 
 /* ENVIAR FORM */
 
+if(formRegister){
 formRegister.addEventListener("submit", function(e) {
            e.preventDefault()
            const userName = document.querySelector("#nombre-register").value
@@ -58,36 +70,59 @@ formRegister.addEventListener("submit", function(e) {
            const profileInfo = JSON.stringify(profileData)
            localStorage.setItem("perfil", perfilInfo)
     window.location.href = "pages/proyectos.html"
-})
+});
+}
 
 /* CERRAR MODAL CON EL FONDO */
 
+if(modalRegister){
 modalRegister.addEventListener("click", function(e) {
     if (e.target === e.currentTarget) {
         modalRegister.classList.remove("active")
     }
-})
+});
+}
 
+if(modalLogin){
 modalLogin.addEventListener("click", function(e) {
     if (e.target === e.currentTarget) {
         modalLogin.classList.remove("active")
     }
-})
+});
+}
 
 /* YA REGISTRADO */
 
 const logged = document.querySelector("#alreadyLogged")
 
+if(logged){
 logged.addEventListener("click", function(e) {
     e.preventDefault()
     modalRegister.classList.remove("active")
     modalLogin.classList.add("active")
-})
+});
+}
 
 const unregistered = document.querySelector("#notRegister")
 
+if(unregistered){
 unregistered.addEventListener("click", function(e) {
     e.preventDefault()
     modalLogin.classList.remove("active")
     modalRegister.classList.add("active")
+});
+}
+
+/* CREAR NUEVO PROYECTO */
+
+newProject.addEventListener("click", function(e) {
+    e.preventDefault()
+
+        console.log("Click");
+
+    modalProject.classList.add("active")
+
+        console.log(modalProject.classList);
+
 })
+
